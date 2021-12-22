bc9f848a494bb01e5271ba2ade5ff195f2eca32f
Toushif <toushif.haq@gmail.com>
2021-12-22 23:30:37 +0530

    canvas pencil aded

--- src/index.js
+++ src/index.js
@@ -60,35 +60,13 @@ function restoreSnapShot() {
 }
 // Added new function to enable canvas drawing 
 function drawPencil(position) {
-    var tool = this;
-    this.started = false;
-
-    // This is called when you start holding down the mouse button.
-    // This starts the pencil drawing.
-    this.mousedown = function (ev) {
-        context.beginPath();
-        context.moveTo(ev._x, ev._y);
-        tool.started = true;
-    };
-
-    // This function is called every time you move the mouse. Obviously, it only
-    // draws if the tool.started state is set to true (when you are holding down
-    // the mouse button).
-    this.mousemove = function (ev) {
-        if (tool.started) {
-            context.lineTo(ev._x, ev._y);
-            context.stroke();
-        }
-    };
-
-    // This is called when you release the mouse button.
-    this.mouseup = function (ev) {
-        if (tool.started) {
-            tool.mousemove(ev);
-            tool.started = false;
-            img_update();
-        }
-    };
+    context.beginPath();
+    context.moveTo(dragStartLocation.x, dragStartLocation.y);
+    context.lineTo(position.x, position.y);
+    context.strokeStyle = 'black';
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
 }
 
 function drawLine(position) {
